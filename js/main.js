a1504828e20ad9fae02d0190b3aa4e29ceba12fb
Figgynewts <znewton@iastate.edu>
2016-09-19 20:42:38 -0500

    Added image carousels

--- js/main.js
+++ js/main.js
@@ -16,3 +16,48 @@ function navbarSetUp(){
 document.addEventListener('DOMContentLoaded', function() {
     navbarSetUp();
 }, false);
+
+
+function createCarousel(carousel){
+    var car = carousel;
+    var next = car.getElementsByClassName('increment')[0];
+    var prev = car.getElementsByClassName('decrement')[0];
+    var slides = car.getElementsByTagName('ol')[0].getElementsByTagName('li');
+    var counter = 0;
+    var amount = slides.length;
+    var current = slides[0];
+    // car.classList.add('active');
+    function navigate(direction) {
+        current.className = '';
+        counter = counter + direction;
+        if (direction === -1 &&
+            counter < 0) {
+            counter = amount - 1;
+        }
+        if (direction === 1 && !slides[counter]) {
+            counter = 0;
+        }
+        current = slides[counter];
+        current.className = 'active';
+    }
+
+    next.addEventListener('click', function (ev) {
+        navigate(1);
+    });
+    prev.addEventListener('click', function (ev) {
+        navigate(-1);
+    });
+    navigate(0);
+
+    if(amount < 2){
+        next.style = 'display: none';
+        prev.style = 'display: none';
+    }
+}
+
+window.onload = function () {
+    carousels = document.getElementsByClassName('carousel');
+    for(var i=0; i < carousels.length; i++){
+        createCarousel(carousels[i]);
+    }
+};
