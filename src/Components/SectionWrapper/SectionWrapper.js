cad85201733d48e45f023df9301992945d33f470
znewton <znewton@iastate.edu>
2017-04-21 15:58:57 -0500

    trying to perfect slide animation

--- src/Components/SectionWrapper/SectionWrapper.js
+++ src/Components/SectionWrapper/SectionWrapper.js
@@ -18,9 +18,13 @@ export default class SectionWrapper extends Component {
   changePage(hash) {
     // Handle no hash
     if(!hash) window.location = '#/';
+    const sw = this.refs.SectionWrapper;
     // Remove #
     hash = hash.slice(1);
     let offset;
+    let currentLeft = sw.style.left.slice(0,-2);
+    currentLeft = !currentLeft ? 0 : currentLeft;
+    let currentTransform = sw.style.transform.slice(11,-3);
     // Match hash to path
     for(offset = 0; offset < this.props.children.length; offset++) {
       let child = this.props.children[offset];
@@ -29,7 +33,11 @@ export default class SectionWrapper extends Component {
     // Handle page not found
     if(offset === this.props.children.length) offset = this.props.children.length-1;
     // Set SectionWrapper Offset to the page
-    this.refs.SectionWrapper.style.marginLeft = -1*100*(offset)+'vw';
+    console.log(offset);
+    let nextLeft = -1*100*(offset);
+    let dir = (1*currentLeft > nextLeft ? 1 : -1);
+    sw.style.left = currentLeft+'vw';
+    sw.style.transform = 'translateX('+ 100*dir*-1+'vw)';
   }
   render() {
     return (
