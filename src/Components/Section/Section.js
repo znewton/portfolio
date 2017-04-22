66aeee117011451e8e08b62efa0f9c19cca64d4e
znewton <znewton@iastate.edu>
2017-04-22 11:04:20 -0500

    Cleaned up transition slide

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 
 export default class Section extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color:(Math.random()*0xFFFFFF<<0).toString(16),
+    }
+  }
+  componentDidMount() {
+    if(window.addEventListener) {
+      if(window.location.hash) {
+        this.changePage(window.location.hash);
+      } else {
+        window.location = '#/';
+      }
+      window.addEventListener('hashchange', (e) => {
+        e.preventDefault();
+        this.changePage(window.location.hash);
+      });
+    }
+  }
+  changePage(hash) {
+    if(this.props.path == hash.slice(1))
+      this.refs.Section.classList.add('active');
+    else {
+      this.refs.Section.classList.remove('active');
+    }
+  }
   render() {
     return (
       <section
+        ref="Section"
         data-path={'#'+this.props.path}
-        className="Section"
-        style={{backgroundColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16)}}
+        className={"Section"+(this.props.active ? ' active' : '')}
+        style={{
+          backgroundColor: '#'+this.state.color,
+        }}
       >
         {this.props.children}
       </section>
@@ -16,4 +45,5 @@ export default class Section extends Component {
 
 Section.propTypes = {
   path: React.PropTypes.string.isRequired,
+  active: React.PropTypes.bool
 }
--- src/Components/SectionWrapper/SectionWrapper.js
+++ src/Components/SectionWrapper/SectionWrapper.js
@@ -1,47 +1,16 @@
 import React, { Component } from 'react';
 
+let previousHash = '';
+
 export default class SectionWrapper extends Component {
-  componentDidMount() {
-    this.refs.SectionWrapper.style.width = this.props.children.length*100+'vw';
-    if(window.addEventListener) {
-      if(window.location.hash) {
-        this.changePage(window.location.hash);
-      } else {
-        window.location = '#/';
-      }
-      window.addEventListener('hashchange', (e) => {
-        e.preventDefault();
-        this.changePage(window.location.hash);
-      });
-    }
-  }
-  changePage(hash) {
-    // Handle no hash
-    if(!hash) window.location = '#/';
-    const sw = this.refs.SectionWrapper;
-    // Remove #
-    hash = hash.slice(1);
-    let offset;
-    let currentLeft = sw.style.left.slice(0,-2);
-    currentLeft = !currentLeft ? 0 : currentLeft;
-    let currentTransform = sw.style.transform.slice(11,-3);
-    // Match hash to path
-    for(offset = 0; offset < this.props.children.length; offset++) {
-      let child = this.props.children[offset];
-      if(child.props.path === hash) break;
-    }
-    // Handle page not found
-    if(offset === this.props.children.length) offset = this.props.children.length-1;
-    // Set SectionWrapper Offset to the page
-    console.log(offset);
-    let nextLeft = -1*100*(offset);
-    let dir = (1*currentLeft > nextLeft ? 1 : -1);
-    sw.style.left = currentLeft+'vw';
-    sw.style.transform = 'translateX('+ 100*dir*-1+'vw)';
-  }
   render() {
     return (
-      <section ref="SectionWrapper" className="SectionWrapper">
+      <section
+        className="SectionWrapper"
+        style={{
+          width: (this.props.children.length*20+80)+'vw'
+        }}
+      >
         {this.props.children}
       </section>
     );
