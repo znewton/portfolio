0943c2965153bc1978b206e89d568d89a0357b38
znewton <znewton@iastate.edu>
2017-05-09 11:49:07 -0500

    Finished converting resume to json

--- src/Pages/Resume/Resume.data.js
+++ src/Pages/Resume/Resume.data.js
@@ -9,7 +9,7 @@ module.exports = {
     portfolio: 'portfolio.znewton.xyz',
   },
   objective: 'Looking for an internship for Summer 2018',
-  education: [
+  educations: [
     { // high school
       name: 'Minnehaha Academy',
       city: 'Minneapolis',
@@ -31,6 +31,136 @@ module.exports = {
       end_year: '2019',
       earned: 'BS in Software Engineering',
       gpa: '3.50/4.00',
+    },
+  ],
+  skills: {
+    languages: [
+      'JavaScript',
+      'PHP',
+      'Java',
+      'HTML/CSS',
+      'SCSS',
+    ],
+    frameworks: [
+      'React',
+      'Angular',
+      'Zend',
+      'Bootstrap',
+      'MySQL',
+    ],
+    software: [
+      'Ubuntu Linux',
+      'Adobe Illustrator',
+      'Adobe Photoshop',
+      'Blender 3D',
+      'MS Office',
+    ],
+  },
+  experiences: [
+    {
+      company_name: 'Workiva',
+      position: 'Front-end Developer Intern',
+      start_month: 'May',
+      start_year: '2017',
+      end_month: 'August',
+      end_year: '2017',
+      actions: [
+        'tbd', 'tbd', 'tbd',
+      ],
+    },
+    {
+      company_name: 'Iowa State University',
+      position: 'Web Developer',
+      start_month: 'February',
+      start_year: '2016',
+      end_month: 'May',
+      end_year: '2017',
+      actions: [
+        'Developed web application to inventory all ISU sites for better maintenance and accessibility regulation',
+        'Rebuilt the university wide website theme for a mobile friendly, accessible user experience',
+        'Collaborated with coworkers on multifaceted projects for various clientele',
+      ],
+    },
+    {
+      company_name: 'Minnehaha Academy',
+      position: 'International Space Station Project',
+      start_month: 'August',
+      start_year: '2014',
+      end_month: 'June',
+      end_year: '2015',
+      actions: [
+        'Converted specs from the mechanical team into a 3d printable bacterial film enclosure chamber',
+        'Sent a micro lab experiment to the ISS to study the effects of artificial shark skin on the growth of bio films in microgravity',
+        'Cooperated between four teams to create a functional, unique experiment in six months',
+      ],
+    },
+    {
+      company_name: 'Minnehaha Academy',
+      position: 'AP Computer Science Teaching Assist.',
+      start_month: 'August',
+      start_year: '2014',
+      end_month: 'December',
+      end_year: '2014',
+      actions: [
+        'Assisted students with in-class work and problem-solving in Java',
+        'Eased the learning curve of an environment that was more individual problem solving than teacher instruction',
+      ],
+    },
+  ],
+  activities: [
+    {
+      name: 'HackISU Midwest Hackathon',
+      dates: ['Fall 2015', 'Fall 2016', 'Spring 2017'],
+      actions: [
+        {
+          description: 'Attempted to create a web app for card game Ultimate Werewolf',
+          date: 'Fall 2015',
+        },
+        {
+          description: 'Created a web app to check songs for FCC profanity violations',
+          date: 'Fall 2016',
+        },
+        {
+          description: 'Tried to build a web app to create a 3D prionting economy at ISU',
+          date: 'Spring 2016',
+        },
+      ],
+    },
+    {
+      name: 'Personal Projects',
+      dates: ['ongoing'],
+      actions: [
+        {
+          description: 'Redesigned a website for my aunt\'s golf club (MNEWGA)',
+          date: 'December 2016',
+        },
+        {
+          description: 'Threw together a beer inventory site for my dad for family get-togethers',
+          date: 'November 2016',
+        },
+        {
+          description: 'Built a 2D game engine in Java from scratch',
+          date: '2015',
+        },
+      ],
+    },
+  ],
+  achievements: [
+    {
+      name: 'HackISU Googliest Hack Award',
+      dates: ['Fall 2016'],
+    },
+    {
+      name: 'Iowa State University Deans List',
+      dates: ['Spring 2016', 'Spring 2017'],
+    },
+    {
+      name: 'Iowa State University Honors Program',
+      dates: ['Fall 2015'],
+    },
+    {
+      name: 'Minnehaha Academy Excellence in STEM Award',
+      dates: ['2015'],
     },
   ],
 }
